bad55e10e1367dec23434e7773ace1ad8a80956b
alphasow <alphaibrahima.sow@outlook.fr>
2024-09-22 13:13:09 +0200

    feat: grid

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,6 +9,42 @@ class Home extends StatelessComponent {
   Iterable<Component> build(BuildContext context) sync* {
     yield const AppHeader(path: '/');
     yield div(
-        classes: "h-full w-full bg-[url('assets/images/bg-image.png')]", []);
+        classes:
+            "h-full w-full flex justify-end items-center bg-[url('assets/images/bg-image.png')]",
+        [
+          div(classes: 'grid grid-cols-2 gap-8 m-10', [
+            ...elements.map((e) {
+              return _Item(title: e.title);
+            })
+          ]),
+        ]);
   }
+}
+
+class _Item extends StatelessComponent {
+  const _Item({required this.title});
+  final String title;
+  @override
+  Iterable<Component> build(BuildContext context) sync* {
+    yield div(classes: 'w-40 bg-[#D9D9D9] flex justify-center items-center ', [
+      h1(classes: 'text-secondary text-xl py-10 px-4 font-black', [
+        text(title),
+      ])
+    ]);
+  }
+}
+
+final elements = <Element>[
+  Element(title: 'Expertise'),
+  Element(title: 'Conseils'),
+  Element(title: 'Ingénierie'),
+  Element(title: 'Etudes'),
+  Element(title: 'Laboratoire'),
+  Element(title: 'Technology & informatique'),
+];
+
+class Element {
+  final String title;
+
+  Element({required this.title});
 }
