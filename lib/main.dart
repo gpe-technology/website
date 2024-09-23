0b83f3803a12dab6dd918c53b7342b1921e1e8c2
alphasow <alphaibrahima.sow@outlook.fr>
2024-09-23 20:49:16 +0200

    font: default system ui

--- lib/main.dart
+++ lib/main.dart
@@ -11,18 +11,6 @@ void main() {
 
   runApp(Document(
     title: 'G.P.E',
-    styles: [
-      css.import('https://fonts.googleapis.com/css?family=Roboto'),
-      css('html, body')
-          .text(
-            fontFamily: const FontFamily.list(
-              [FontFamily('Roboto'), FontFamilies.sansSerif],
-            ),
-          )
-          .box(width: 100.percent, minHeight: 100.vh)
-          .box(margin: EdgeInsets.zero, padding: EdgeInsets.zero),
-      css('h1').text(fontSize: 4.rem).box(margin: EdgeInsets.unset),
-    ],
     head: [
       link(rel: 'stylesheet', href: 'styles.css'),
       link(rel: 'manifest', href: 'manifest.json'),
